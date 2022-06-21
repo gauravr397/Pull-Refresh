2bfcaac4dd079e9052aa76c798865385c5fc7d36
LazyPanda397 <gauravr397@gmail.com>
2022-06-21 13:17:17 +0530

    pull ->

--- app/src/main/java/com/example/pullrefresh/MainActivity.kt
+++ app/src/main/java/com/example/pullrefresh/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,13 +18,28 @@ class MainActivity : AppCompatActivity() {
 
         val adaptor = RVAdaptor(item)
         recyclerView.adapter = adaptor
+
+        val swipe : SwipeRefreshLayout = findViewById(R.id.swiperefrsh)
+        swipe.setOnRefreshListener {
+            item.addAll(updateData())
+            adaptor.notifyDataSetChanged()
+            swipe.isRefreshing=false
+        }
     }
 
     fun fetchData() : ArrayList<String>{
         val list = ArrayList<String>()
         for(i in 0 until  1){
-            list.add("item $i")
+            list.add("Old Text")
         }
         return list
     }
-} 
+
+    fun updateData() :ArrayList<String>{
+        val item = ArrayList<String>()
+            for (j in 1 until 2) {
+                item.add("New Text 🔥")
+            }
+        return item
+    }
+}
